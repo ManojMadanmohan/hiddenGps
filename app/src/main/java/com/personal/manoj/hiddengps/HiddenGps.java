647aacd36de98665ac2b7d94bb28d6c364956676
Manoj <manoj@zophop.com>
2017-09-03 20:04:13 +0530

    service started

--- app/src/main/java/com/personal/manoj/hiddengps/HiddenGps.java
+++ app/src/main/java/com/personal/manoj/hiddengps/HiddenGps.java
@@ -1,6 +1,8 @@
 package com.personal.manoj.hiddengps;
 
 import android.app.Application;
+import android.content.Intent;
+import android.widget.Toast;
 
 public class HiddenGps extends Application
 {
@@ -8,6 +10,8 @@ public class HiddenGps extends Application
     public void onCreate()
     {
         super.onCreate();
+        Toast.makeText(this, "application onreate called", Toast.LENGTH_SHORT).show();
         //start service to listen to location
+        startService(new Intent(this, LocationTrackingService.class));
     }
 }
